b57c5c6e908001436e72c5571ceba3f085406d26
Sergey V. Elfimov <46016480+SergeiVElfimov@users.noreply.github.com>
2025-06-27 11:36:17 +0300

    PR issues #5 (#6)
    * feat: remove fastapi and  sqlalchemy from dependency with associated code.
    
    * fix lock file and readme

--- fastapi_s3_storages/constants.py
+++
@@ -1,5 +0,0 @@
-MAPPING_MIME_TYPE_AND_FORMAT_FILE = {
-    ".msg": "application/vnd.ms-outlook",
-}
-
-DEFUALT_MIME_TYPE = "text/plain"
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,12 @@ from __future__ import annotations
 import typing
 
 import pytest
-from httpx import AsyncClient
 
-from fastapi_s3_storages import S3Storage
+from aio_s3_storages import S3Storage
 
 pytest_plugins = ("tests.pytest.plugin",)
 
 if typing.TYPE_CHECKING:
-    from collections.abc import AsyncGenerator
-
-    from fastapi import FastAPI
     from pytest_mock import MockerFixture
     from types_aiobotocore_s3.client import S3Client
 
@@ -24,9 +20,3 @@ def s3_storage_client_mock(s3_client: S3Client, bucket_name: str, mocker: Mocker
     mocker.patch.object(S3Storage, "client", mocker.MagicMock(return_value=client_mock))
     S3Storage.bucket_name = bucket_name
     return S3Storage
-
-
-@pytest.fixture
-async def http_client(web_app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=web_app, base_url="http://test", timeout=2.0) as ac:
-        yield ac
